8e4c90f3cbd03243fd357a25d411dc2650b22a31
Andre <andreraizdetudo@gmail.com>
2021-08-27 21:54:02 -0300

    calculadora-display-funcionando
    Display-funcionando

--- calculadora/calculadora/src/main/Calculator.jsx
+++ calculadora/calculadora/src/main/Calculator.jsx
@@ -1,25 +1,29 @@
 import React, {Component} from 'react'
 import './Calculator.css'
 import Button from '../components/Button'
+import Display from'../components/Display'
 
 export default class Calculator extends Component{
     render(){
         return(
             <div className="calculator">
-                <Button label="AC" />
+                <Display value="100" />
+                <Button className="button" label="AC" />
+                <Button label="9"/>
+                <Button label="8"/>
                 <Button label=":"/>
                 <Button label="7"/>
-                <Button label="8"/>
-                <Button label="9"/>
-                <Button label="X"/>
-                <Button label="5"/>
                 <Button label="6"/>
-                <Button label="-"/>
-                <Button label="2"/>
+                <Button label="5"/>
+                <Button label="X"/>
+                <Button label="4"/>
                 <Button label="3"/>
-                <Button label="+"/>
+                <Button label="2"/>
+                <Button label="-"/>
+                <Button label="1"/>
                 <Button label="0"/>
                 <Button label="."/>
+                <Button label="+"/>
                 <Button label="="/>
                 
             </div>
