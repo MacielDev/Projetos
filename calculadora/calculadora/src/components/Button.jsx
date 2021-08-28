26196bca5cee124b82f35be3362b7d5ddb193eb6
Andre <andreraizdetudo@gmail.com>
2021-08-28 10:43:03 -0300

    Formatação dois botões "AC","0" e "Operações"
    Implementação dos estilo dos botões.

--- calculadora/calculadora/src/components/Button.jsx
+++ calculadora/calculadora/src/components/Button.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
+
 import React from 'react'
 import Button from './Button.css'
 
 
 export default props =>{
-    let classes ='button'
-    classes += props.operation ? 'operation':''
-    classes += props.double ? 'double':''
-    classes += props.triple ? 'triple':''
+    var classes ='button'
+    classes += props.operation ? ' operation':''
+    classes += props.double ? ' double':''
+    classes += props.triple ? ' triple':''
     return (
         <button 
             onClick={e => props.click && props.click(props.label)}
