9791d39b2ec906a856af5fccda2cfc118069834f
Andre <andreraizdetudo@gmail.com>
2021-10-10 15:11:41 -0300

    Refatorando a aplicação

--- Projeto Electra/universidade_corporativa/src/App.js
+++ Projeto Electra/universidade_corporativa/src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import Headers from './components/header'
-import HeaderPrincipal from './components/headerPrincipal'
-import BanerPrincipal from './components/banerPrincipal'
-import Footer from './components/footer'
+
 function App (props){
   return(
-    <div>
-        <Headers /> 
-        <HeaderPrincipal />
-        <BanerPrincipal />
-        <Footer /> 
-      </div>
+    <h1>Do zero</h1>
   )
 }
 export default App
