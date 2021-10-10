0e9f0a16f4d9cf76941f2a8e11bd1aa475c5edc3
Andre <andreraizdetudo@gmail.com>
2021-10-10 12:13:39 -0300

    alterações-ajuste

--- Projeto Electra/universidade_corporativa/src/components/banerPrincipal/index.js
+++ Projeto Electra/universidade_corporativa/src/components/banerPrincipal/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Jumbotron, Container } from "reactstrap";
 import banerPrincipal from "./imagens/banerPrincipal .png";
-import estilo from'./estilo.css'
+
 const BanerPrincipal = () => {
   return (
     <div>
@@ -10,7 +10,6 @@ const BanerPrincipal = () => {
           <img id="imagemPrincipal"
             src={banerPrincipal}
             alt="Imagem sobre  T.I Principal"
-            className="imagemPrincipal"
           />
         </Container>
       </Jumbotron>
