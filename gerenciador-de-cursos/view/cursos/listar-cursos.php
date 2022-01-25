dbdf93034beec356c37539826015997d2e1379ab
MacielDev <andreraizdetudo@gmail.com>
2022-01-24 21:30:39 -0300

    Funcionalidade de exclusão funcionando

--- gerenciador-de-cursos/view/cursos/listar-cursos.php
+++ gerenciador-de-cursos/view/cursos/listar-cursos.php
@@ -2,8 +2,10 @@
 <a href="/novo-curso" class="btn btn-primary mb-2">Novo curso</a>
 <ul class="list-group">
     <?php foreach ($cursos as $curso) : ?>
-        <li class="list-group-item">
+        <li class="list-group-item d-flex justify-content-between">
             <?= $curso->getDescricao(); ?>
+            <a class="btn btn-danger btn-sm" 
+            href="/excluir-curso?id=<?=$curso->getId();?>">Excluir</a>
         </li>
     <?php endforeach; ?>
 </ul>
