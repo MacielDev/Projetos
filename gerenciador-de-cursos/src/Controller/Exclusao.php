4bf7e566df5b360b82bc9b38e0d04613c9cfddde
MacielDev <andreraizdetudo@gmail.com>
2022-01-26 21:47:19 -0300

    Mensagens através de trait

--- gerenciador-de-cursos/src/Controller/Exclusao.php
+++ gerenciador-de-cursos/src/Controller/Exclusao.php
@@ -3,12 +3,14 @@
 namespace Alura\Cursos\Controller;
 
 use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Helper\FlashMessageTrait;
 use Alura\Cursos\Infra\EntityManagerCreator;
 use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
 class Exclusao implements InterfaceControladorRequisicao
-{   
-    
+{
+
+    use FlashMessageTrait;
     private $entityManager;
     public function __construct()
     {
@@ -21,16 +23,20 @@ class Exclusao implements InterfaceControladorRequisicao
             'id',
             FILTER_VALIDATE_INT
         );
-      
-        if(is_null($id) || $id === false){
+
+        if (is_null($id) || $id === false) {
+            $this->defineMensagem('danger','Curso inexistente');
+            
             header('location: /listar-cursos');
             return;
         }
         $curso = $this->entityManager->getReference(Curso::class, $id);
-        
+
         $this->entityManager->remove($curso);
         $this->entityManager->flush();
+        $this->defineMensagem('success','Curso excluido com sucesso');
         
+
         header('Location:/listar-cursos');
     }
 }
--- gerenciador-de-cursos/src/Controller/Persistencia.php
+++ gerenciador-de-cursos/src/Controller/Persistencia.php
@@ -3,12 +3,13 @@
 namespace Alura\Cursos\Controller;
 
 use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Helper\FlashMessageTrait;
 use Alura\Cursos\Infra\EntityManagerCreator;
 use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
 class Persistencia implements InterfaceControladorRequisicao
 {
-
+    use FlashMessageTrait;
     /**
      * @var \Doctrine\ORM\EntityManagerInterface
      */
@@ -40,10 +41,14 @@ class Persistencia implements InterfaceControladorRequisicao
         if (!is_null($id) && $id !== false) {
             $curso->setId($id);
             $this->entityManager->merge($curso);
+            $_SESSION['mensagem'] = 'Curso atualizado com sucesso';
         } else {
             // Inserir no banco 
             $this->entityManager->persist($curso);
+            $_SESSION['mensagem'] = 'Curso criado com sucesso';
         }
+        $_SESSION['tipo_mensagem'] = 'success';
+        
         $this->entityManager->flush();
         //UTILIZANDO UM CABEÇALHO PHP PARA REDIRECIONAR O BROWSER PARA OUTRA PÁGINA
         header('Location:/listar-cursos', true, 302);
