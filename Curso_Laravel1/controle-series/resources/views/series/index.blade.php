07447a050aef0fbe83d8d3e7b19da20b40e006ff
MacielDev <andreraizdetudo@gmail.com>
2022-02-06 20:00:34 -0300

    Iniciando processo de exclusão

--- Curso_Laravel1/controle-series/resources/views/series/index.blade.php
+++ Curso_Laravel1/controle-series/resources/views/series/index.blade.php
@@ -5,20 +5,21 @@ Séries
 @endsection
 
 @section('conteudo')
-        @if(!empty($mensagem))
-        <div class="alert alert-success p-md-4" role="alert">{{$mensagem}}</div>
-        @endif
-        <a href="/series/criar" class="btn btn-dark btn-lg mb-1">Adicionar</a>
-        
-        <ul class="list-group">
-            @foreach ($series as $serie)
-            <li class="list-group-item  d-flex justify-content-between">{{ $serie->nome }}
-                <span class="">
-                    <a href="#" class="btn btn-info">Editar</a>
-                    <a href="#" class="btn btn-danger">Excluir</a>
-                </span>
-            </li>
-            @endforeach
-        </ul>
-@endsection
+@if(!empty($mensagem))
+<div class="alert alert-success p-md-4" role="alert">{{$mensagem}}</div>
+@endif
+<a href="/series/criar" class="btn btn-dark btn-lg mb-1">Adicionar</a>
 
+<ul class="list-group">
+    @foreach ($series as $serie)
+    <li class="list-group-item  d-flex justify-content-between">{{ $serie->nome }}
+        <span class="d-flex flex-row">
+            <a href="#" class="btn btn-info ">Editar</a>
+            <form method="post" action="/series/remover id=?">
+                <button href="#" class="btn btn-danger">Excluir</button>
+            </form>
+        </span>
+    </li>
+    @endforeach
+</ul>
+@endsection
