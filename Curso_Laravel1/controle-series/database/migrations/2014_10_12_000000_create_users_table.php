490b460ab8bb1e8e344ad5e2412d893cba5786b3
Andre <andreraizdetudo@gmail.com>
2022-03-16 22:07:03 -0300

    Ultimo commit antes de implementar autenticacao)

--- Curso_Laravel1/controle-series/database/migrations/2014_10_12_000000_create_users_table.php
+++ Curso_Laravel1/controle-series/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password',60);
+            $table->rememberToken('remember_token',100)->nullable();
             $table->timestamps();
         });
     }
